f4e4dffe1091b1f01b0284c2b57477bcbe992d4f
shelbin011 <shelbincbino234@gmail.com>
2025-11-23 19:04:23 +0530

    git pust (trying to host)

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jmz77k^=@9tmuo5+!xuop)yji@8x_0x-*$6x39+49xej34glmu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 
 # Application definition
